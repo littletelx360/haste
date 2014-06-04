ac14bde5c47ef6f9d12cb761398b4d9f9492a5d5
Jarrod Connolly <jarrod@nestedquotes.ca>
2014-06-03 17:08:07 -0700

    started option parsing

--- bin/haste.js
+++ bin/haste.js
@@ -2,3 +2,50 @@
 'use strict';
 
 var packageJson = require(__dirname + '/../package.json');
+var parseArgs = require('minimist');
+
+var paramOptions = {
+    boolean: [
+        'version',
+        'help'
+    ],
+    string: [
+        'url'
+    ],
+    alias: {
+        'v': 'version',
+        'h': 'help',
+        'u': 'url'
+    },
+    default: {
+        version: false,
+        help: false
+    }
+};
+
+var argv = parseArgs(process.argv.slice(2), paramOptions);
+
+if(argv.help === true) {
+    showUsageHelp();
+}
+
+if(argv.version === true) {
+    console.log('haste version: %s', packageJson.version);
+    process.exit(0);
+}
+
+if(!argv.url) {
+    showUsageHelp();
+}
+
+console.log("running...");
+//console.dir(argv);
+
+function showUsageHelp()
+{
+    console.log('Usage: haste [options]');
+    console.log('   --help           Show this help');
+    console.log('   --version        Show version number');
+    console.log('   --url            Add a URL step to the test');
+    process.exit(1);
+}
